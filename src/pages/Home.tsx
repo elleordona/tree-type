6006aa5e93c045eefeb5ecc3df1dcb8ba4efbdbd
Elle Ordoña <eto0507@hotmail.co.uk>
2025-11-13 17:03:14 +0000

    theme toggle component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-            <h1>Home Page</h1>
             {/* Theme Toggle */}
+            <ThemeToggle />
 
             {/* Navbar */}
 
